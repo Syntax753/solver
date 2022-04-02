6553e54662ab516333a674759054038a8805a211
Peter Turner <peter.turner@hcl.com>
2022-04-02 18:35:23 +0100

    - d1_000_000 solution - not 100% it will pass the overflow tests but have switched to long to cater for that - add Solver helper functions for processing arrays based on long/int arrays of inputs

--- src/main/java/y2022/qualification/d1_000_000/Solution.java
+++ src/main/java/y2022/qualification/d1_000_000/Solution.java
@@ -1,11 +1,12 @@
+package y2022.qualification.d1_000_000;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Solver {
+public class Solution {
     private static String solve(int[] in) {
-        // TODO: representative default operation
         Arrays.sort(in);
 
         int cnt = 0;
@@ -20,7 +21,6 @@ public class Solver {
     }
 
     private static String solve(long[] in) {
-        // TODO: representative default operation
         Arrays.sort(in);
 
         int cnt = 0;
@@ -39,9 +39,14 @@ public class Solver {
 
         int T = sc.nextInt();
         for (int i = 1; i <= T; i++) {
-            int[] in = new int[]{1, 2};
+            int N = sc.nextInt();
 
-            System.out.println("Case #" + i + ": " + solve(in));
+            int[] dice = new int[N];
+            while (--N >= 0) {
+                dice[N] = sc.nextInt();
+            }
+
+            System.out.println("Case #" + i + ": " + solve(dice));
         }
 
         sc.close();
