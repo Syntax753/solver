5e83810c49cf7fbf9808458354dbf71ab8fbdbe4
Peter Turner <peter.turner@hcl.com>
2022-04-03 15:43:42 +0100

    - chainreactions start - not complete - need graph functionality - need to work out how to standardise greedy algorithms

--- src/main/java/y2022/qualification/chainreations/Solution.java
+++ src/main/java/y2022/qualification/chainreations/Solution.java
@@ -1,21 +1,42 @@
+package y2022.qualification.chainreations;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Solver {
+public class Solution {
+
+    // TODO Add Node support for graph problems
+    // private class Node
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
         int T = sc.nextInt();
         for (int i = 1; i <= T; i++) {
-            int[] in = new int[]{1, 2};
+            int C = sc.nextInt();
+            System.out.println(C);
 
-            System.out.println("Case #" + i + ": " + solve(in));
+            int[] N = new int[C];
+            for (int j = 0; j < C; j++) {
+                N[j] = sc.nextInt();
+            }
+
+            int[] E = new int[C];
+            for (int j = 0; j < C; j++) {
+                E[j] = sc.nextInt();
+            }
+
+            System.out.println("Case #" + i + ": " + solve(N, E));
         }
 
         sc.close();
+    }
+
+    private static String solve(int[] nodes, int[] edges) {
+
+        return Arrays.toString(nodes) + " " +Arrays.toString(edges);
     }
 
     private static String solve(int[] in) {
